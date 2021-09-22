1d8f6ff1798306916288c6e85909a7675d114561
feles-ao42 <ao.42ao.42@gmail.com>
2021-09-22 16:15:43 +0900

    fix sql

--- onserver/API/rest.py
+++ onserver/API/rest.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, abort, jsonify,Flask
 import pymysql.cursors
 import json
-
+import re
 app = Flask(__name__)
 
 @app.route('/test')
@@ -26,8 +26,14 @@ def snr():
     enc = json.dumps(result)
     return enc
 
-@app.route('/snr/<list:xyz>', methods=['GET'])
+@app.route('/snr/<str:xyz>', methods=['GET'])
 def snrfind(xyz=None):
+    num = re.findall("'(.*)'-", xyz)
+    fnum = [float(n) for n in num]
+    ax = fnum[0]
+    num = re.findall("-'(.*)'", xyz)
+    fnum = [float(n) for n in num]
+    ay = fnum[0]
     conn = pymysql.connect(
         host='localhost',
         user='feles5g',
@@ -36,8 +42,8 @@ def snrfind(xyz=None):
         password='local5g',
         cursorclass=pymysql.cursors.DictCursor)
     with conn.cursor() as cursor:
-        sql = "SELECT lat,lon,alt,SNR FROM gndr_main where %s < lat and lat < %s"
-        cursor.execute(sql,xyz[0],xyz[1])
+        sql = "SELECT lat,lon,alt,SNR FROM gndr_main where %f < lat and lat < %f"
+        cursor.execute(sql,ax,ay)
         result = cursor.fetchall()
     enc = json.dumps(result)
     return enc
