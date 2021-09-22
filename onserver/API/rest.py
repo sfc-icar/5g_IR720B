3beca9ce8af641150adf8d2d61ce1c014085052a
feles-ao42 <ao.42ao.42@gmail.com>
2021-09-22 15:27:17 +0900

    delete comment

--- onserver/API/rest.py
+++ onserver/API/rest.py
@@ -5,11 +5,8 @@ import json
 from pymongo import DESCENDING
 import pymongo
 
-# api Blueprint作成　http://host/api 以下のものはここのルールで処理される
 api = Blueprint('api', __name__, url_prefix='/api')
 
-# エンドポイント http:/host/api/zyz, GETメソッドのみ受け付ける
-# routeは複数指定も可能、methodsはリストなので複数指定可能
 @api.route('/xyz', methods=['GET'])
 def list_user():
     conn = pymysql.connect(
@@ -26,21 +23,16 @@ def list_user():
     enc = json.dumps(result)
     return enc
 
-# URL中のパラメータ取得 <type:variable_name>
-# type: string(default), int, float, path, uuid
-# variable_name: 変数名
+
 #@api.route('/xyz/<int:user_id>', methods=['GET'])
 #def get_user(user_id=None):
 #    user = User.query.filter_by(id=user_id).first()
 #    return jsonify(user.to_dict())
 
-# エラーのハンドリング errorhandler(xxx)を指定、複数指定可能
-# ここでは400,404をハンドリングする
+
 @api.errorhandler(400)
 @api.errorhandler(404)
 def error_handler(error):
-    # error.code: HTTPステータスコード
-    # error.description: abortで設定したdict型
     return jsonify({'error': {
         'code': error.description['code'],
         'message': error.description['message']
