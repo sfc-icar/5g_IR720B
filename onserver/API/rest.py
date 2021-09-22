7347a77a0757174e9a9221d564e5022f0f78ffb7
feles-ao42 <ao.42ao.42@gmail.com>
2021-09-22 17:57:35 +0900

    fix sql

--- onserver/API/rest.py
+++ onserver/API/rest.py
@@ -26,24 +26,24 @@ def snrall():
     enc = json.dumps(result)
     return enc
 
-@app.route('/snrfind', methods=['GET'])
-def snrfind(ax=None,bx=None):
-    ax = float(request.args.get('ax', 0))
-    bx = float(request.args.get('bx', 1))
-    a=[ax,bx]
-    conn = pymysql.connect(
-        host='localhost',
-        user='feles5g',
-        db='5gfeles',
-        charset='utf8mb4',
-        password='local5g',
-        cursorclass=pymysql.cursors.DictCursor)
-    with conn.cursor() as cursor:
-        sql = "SELECT lat,lon,alt,SNR FROM gndr_main where %s < lat and lat < %s ;"
-        cursor.execute(sql,(a[0],a[1]))
-        result = cursor.fetchall()
-    enc = json.dumps(result)
-    return enc
+# @app.route('/snrfind', methods=['GET'])
+# def snrfind(ax=None,bx=None):
+#     ax = float(request.args.get('ax', 0))
+#     bx = float(request.args.get('bx', 1))
+#     a=[ax,bx]
+#     conn = pymysql.connect(
+#         host='localhost',
+#         user='feles5g',
+#         db='5gfeles',
+#         charset='utf8mb4',
+#         password='local5g',
+#         cursorclass=pymysql.cursors.DictCursor)
+#     with conn.cursor() as cursor:
+#         sql = "SELECT lat,lon,alt,SNR FROM gndr_main where %s < lat and lat < %s ;"
+#         cursor.execute(sql,(a[0],a[1]))
+#         result = cursor.fetchall()
+#     enc = json.dumps(result)
+#     return enc
 
 @app.errorhandler(400)
 @app.errorhandler(404)
