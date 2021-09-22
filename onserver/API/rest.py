9b9a31a414145791c3a3e4f0f86474815825d52f
feles-ao42 <ao.42ao.42@gmail.com>
2021-09-22 16:05:33 +0900

    fix sql

--- onserver/API/rest.py
+++ onserver/API/rest.py
@@ -26,6 +26,22 @@ def snr():
     enc = json.dumps(result)
     return enc
 
+@app.route('/snr/<list:xyz>', methods=['GET'])
+def snrfind(xyz=None):
+    conn = pymysql.connect(
+        host='localhost',
+        user='feles5g',
+        db='5gfeles',
+        charset='utf8mb4',
+        password='local5g',
+        cursorclass=pymysql.cursors.DictCursor)
+    with conn.cursor() as cursor:
+        sql = "SELECT lat,lon,alt,SNR FROM gndr_main where %s < lat and lat < %s"
+        cursor.execute(sql,xyz[0],xyz[1])
+        result = cursor.fetchall()
+    enc = json.dumps(result)
+    return enc
+
 @app.errorhandler(400)
 @app.errorhandler(404)
 def error_handler(error):
