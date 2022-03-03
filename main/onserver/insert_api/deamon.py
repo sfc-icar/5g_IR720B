e2bdfb8420df75de99129c7343f3b0c0be823453
feles-ao42 <ao.42ao.42@gmail.com>
2022-03-03 16:16:00 +0900

    websocktesに変更

--- main/onserver/insert_api/deamon.py
+++ main/onserver/insert_api/deamon.py
@@ -4,52 +4,36 @@ import json
 import time
 
 import pymysql.cursors
-from websocket_server import WebsocketServer
 
+import asyncio
+import websockets
 
-class Websocket_Server:
-
-    def __init__(self, host, port):
-        self.server = WebsocketServer(port, host=host)
-
-    def new_client(self, client, server):
-        self.server.send_message_to_all("hey all, a new client has joined us")
-
-    # クライアントからメッセージを受信したときに呼ばれる関数
-    def message_received(self, client, server, message):
+async def echo(websocket):
+    async for message in websocket:
         row = json.loads(message)
         print(row)
-        conn = pymysql.connect(
-            host='localhost',
-            user='feles5g',
-            db='5gfeles',
-            charset='utf8mb4',
-            password='local5g',
-            cursorclass=pymysql.cursors.DictCursor)
-        for num in range(len(row)):
-            if row[num] == "n/a":
-                row[num] = None
-        with conn.cursor() as cursor:
-            sql = "INSERT INTO vg_usb_main(time, lat, lon, alt, MMC, MNC, cell_id, earfcn_dl, earfcn_ul, RSRP, RSRQ, SINR, LTE_RRC, csq, cgreg) VALUES ( % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s);"
-            cursor.execute(sql, (row[0], row[1], row[2], row[3], row[4], row[5],
-                                 row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14]))
-        conn.commit()
-        conn.close()
+        # conn = pymysql.connect(
+        #     host='localhost',
+        #     user='feles5g',
+        #     db='5gfeles',
+        #     charset='utf8mb4',
+        #     password='local5g',
+        #     cursorclass=pymysql.cursors.DictCursor)
+        # for num in range(len(row)):
+        #     if row[num] == "n/a":
+        #         row[num] = None
+        # with conn.cursor() as cursor:
+        #     sql = "INSERT INTO vg_usb_main(time, lat, lon, alt, MMC, MNC, cell_id, earfcn_dl, earfcn_ul, RSRP, RSRQ, SINR, LTE_RRC, csq, cgreg) VALUES ( % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s);"
+        #     cursor.execute(sql, (row[0], row[1], row[2], row[3], row[4], row[5],
+        #                          row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14]))
+        # conn.commit()
+        # conn.close()
 
-    def run(self):
-        self.server.set_fn_new_client(self.new_client)
-        self.server.set_fn_message_received(self.message_received)
-        self.server.run_forever()
+async def main():
+    async with websockets.serve(echo, "203.178.143.13", 5111):
+        await asyncio.Future()  # run forever
 
-
-def main():
-    IP_ADDR = "203.178.143.13"  # IPアドレスを指定
-    PORT = 5111  # ポートを指定
-    ws_server = Websocket_Server(IP_ADDR, PORT)
-    ws_server.run()
-
+asyncio.run(main())
 
 if __name__ == '__main__':
-    while True:
-        main()
-        time.sleep(1)
+    asyncio.run(main())
