dad1e4469468b837fdda8a1551bdc40257e873aa
feles-ao42 <ao.42ao.42@gmail.com>
2022-03-03 16:42:55 +0900

    コメントアウトはずし

--- main/onserver/insert_api/deamon.py
+++ main/onserver/insert_api/deamon.py
@@ -12,22 +12,22 @@ async def echo(websocket):
     async for message in websocket:
         row = json.loads(message)
         print(row)
-        # conn = pymysql.connect(
-        #     host='localhost',
-        #     user='feles5g',
-        #     db='5gfeles',
-        #     charset='utf8mb4',
-        #     password='local5g',
-        #     cursorclass=pymysql.cursors.DictCursor)
-        # for num in range(len(row)):
-        #     if row[num] == "n/a":
-        #         row[num] = None
-        # with conn.cursor() as cursor:
-        #     sql = "INSERT INTO vg_usb_main(time, lat, lon, alt, MMC, MNC, cell_id, earfcn_dl, earfcn_ul, RSRP, RSRQ, SINR, LTE_RRC, csq, cgreg) VALUES ( % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s);"
-        #     cursor.execute(sql, (row[0], row[1], row[2], row[3], row[4], row[5],
-        #                          row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14]))
-        # conn.commit()
-        # conn.close()
+        conn = pymysql.connect(
+            host='localhost',
+            user='feles5g',
+            db='5gfeles',
+            charset='utf8mb4',
+            password='local5g',
+            cursorclass=pymysql.cursors.DictCursor)
+        for num in range(len(row)):
+            if row[num] == "n/a":
+                row[num] = None
+        with conn.cursor() as cursor:
+            sql = "INSERT INTO vg_usb_main(time, lat, lon, alt, MMC, MNC, cell_id, earfcn_dl, earfcn_ul, RSRP, RSRQ, SINR, LTE_RRC, csq, cgreg) VALUES ( % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s);"
+            cursor.execute(sql, (row[0], row[1], row[2], row[3], row[4], row[5],
+                                 row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14]))
+        conn.commit()
+        conn.close()
 
 async def main():
     async with websockets.serve(echo, "203.178.143.13", 5111):
