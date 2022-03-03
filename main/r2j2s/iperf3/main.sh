a802269b98306f92107385e2b5f98a4c000e5a02
feles-ao42 <ao.42ao.42@gmail.com>
2022-03-03 10:19:52 +0900

    ip修正 bandwidth追記

--- main/r2j2s/iperf3/main.sh
+++ main/r2j2s/iperf3/main.sh
@@ -1,2 +1,2 @@
-nohup iperf3 -c 118.27.19.228 -t 100 --port 50002 -u > normal.txt &
-nohup iperf3 -c 118.27.19.228 -t 100 --port 50001 -u -R > reverse.txt &
+nohup iperf3 -c 203.178.143.13 -t 100 --port 50002 -u --bandwidth 300M > normal.txt &
+nohup iperf3 -c 203.178.143.13 -t 100 --port 50001 -u -R --bandwidth 300M > reverse.txt &
